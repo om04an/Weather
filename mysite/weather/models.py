8228bfff58f0fe47d06054b8cbad66f263e8bf50
om04an <mivanpass@gmail.com>
2023-01-20 04:25:03 +0300

    Create DataBase model and create base API

--- mysite/weather/models.py
+++ mysite/weather/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    city = models.CharField(max_length=200)
+    temperature = models.CharField(max_length=200)
+    temperature_feelslike = models.CharField(max_length=200)
+    precipitation = models.CharField(max_length=200)
+    wind = models.CharField(max_length=200)
+    cloudiness = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Town"
+
+    def __str__(self):
+        return self.city
